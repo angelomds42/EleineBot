124aaee06555f32a058931f57b54fb3da1a9274f
angelomds42 <angelomds42@gmail.com>
2025-05-13 15:14:33 -0300

    refactor(afk): simplify `CheckAFKMiddleware`
    - Extracted getMessageFromUpdate, isGroupChat, isAFKCommand, handleReturnFromAFK and handleMentionedAFK to
    reduce complexity and improve readability.
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- internal/modules/afk/handlers.go
+++ internal/modules/afk/handlers.go
@@ -20,127 +20,132 @@ func WithLargeMediaPreview(p *bot.SendMessageParams) {
 
 func CheckAFKMiddleware(next bot.HandlerFunc) bot.HandlerFunc {
 	return func(ctx context.Context, b *bot.Bot, update *models.Update) {
-		message := update.Message
-
-		if update.Message == nil {
-			if update.CallbackQuery == nil {
-				next(ctx, b, update)
-				return
-			}
-
-			message = update.CallbackQuery.Message.Message
-
-			if update.CallbackQuery.Message.Type == 1 || message == nil {
-				next(ctx, b, update)
-				return
-			}
+		message := getMessageFromUpdate(update)
+		if message == nil || message.From == nil {
+			next(ctx, b, update)
+			return
 		}
 
-		if message.From == nil ||
-			message.Chat.Type != models.ChatTypeGroup &&
-				message.Chat.Type != models.ChatTypeSupergroup ||
-			regexp.MustCompile(`^/\bafk\b|^\bbrb\b`).MatchString(message.Text) {
+		if !isGroupChat(message.Chat.Type) || isAFKCommand(message.Text) {
 			next(ctx, b, update)
 			return
 		}
 
 		mentionedUserID := getUserIDFromMessage(message)
-		if !user_is_away(message.From.ID) && !user_is_away(mentionedUserID) {
+
+		userID := message.From.ID
+		userAFK := user_is_away(userID)
+		mentionedAFK := user_is_away(mentionedUserID)
+
+		if !userAFK && !mentionedAFK {
 			next(ctx, b, update)
 			return
 		}
 
-		i18n := localization.Get(update)
-
-		if user_is_away(message.From.ID) {
-			_, duration, err := get_user_away(message.From.ID)
-			if err != nil && err != sql.ErrNoRows {
-				slog.Error("Couldn't get user away status",
-					"UserID", message.From.ID,
-					"Error", err.Error())
-				return
-			}
-
-			humanizedDuration := localization.HumanizeTimeSince(duration, update)
-			if err = unset_user_away(message.From.ID); err != nil {
-				slog.Error("Couldn't unset user away status",
-					"UserID", message.From.ID,
-					"Error", err.Error())
-				return
-			}
-
-			utils.SendMessage(ctx, b, message.Chat.ID, message.ID,
-				i18n("now-available", map[string]interface{}{
-					"userID":        message.From.ID,
-					"userFirstName": utils.EscapeHTML(message.From.FirstName),
-					"duration":      humanizedDuration,
-				}),
-				WithLargeMediaPreview,
-			)
+		if userAFK {
+			handleReturnFromAFK(ctx, b, update, message, userID)
 		}
 
-		if mentionedUserID != 0 && user_is_away(mentionedUserID) {
-			reason, duration, err := get_user_away(mentionedUserID)
-			if err != nil && err != sql.ErrNoRows {
-				slog.Error("Couldn't get user away status",
-					"UserID", mentionedUserID,
-					"Error", err.Error())
-				return
-			}
-
-			humanizedDuration := localization.HumanizeTimeSince(duration, update)
-			user, err := b.GetChat(ctx, &bot.GetChatParams{ChatID: mentionedUserID})
-			if err != nil {
-				slog.Error("Couldn't get user",
-					"UserID", mentionedUserID,
-					"Error", err.Error())
-				return
-			}
-
-			text := i18n("user-unavailable", map[string]interface{}{
-				"userID":        mentionedUserID,
-				"userFirstName": utils.EscapeHTML(user.FirstName),
-				"duration":      humanizedDuration,
-			})
-
-			if reason != "" {
-				text += "\n" + i18n("user-unavailable-reason", map[string]interface{}{
-					"reason": reason,
-				})
-			}
-
-			utils.SendMessage(ctx, b, message.Chat.ID, message.ID, text,
-				WithLargeMediaPreview,
-			)
+		if mentionedUserID != 0 && mentionedAFK {
+			handleMentionedAFK(ctx, b, update, message, mentionedUserID)
 		}
 
 		next(ctx, b, update)
 	}
 }
 
-func getUserIDFromMessage(message *models.Message) int64 {
-	if message.ReplyToMessage != nil && message.ReplyToMessage.From != nil {
-		return message.ReplyToMessage.From.ID
+func getMessageFromUpdate(update *models.Update) *models.Message {
+	if update.Message != nil {
+		return update.Message
+	}
+	if update.CallbackQuery != nil && update.CallbackQuery.Message.Type != 1 {
+		return update.CallbackQuery.Message.Message
+	}
+	return nil
+}
+
+func isGroupChat(chatType models.ChatType) bool {
+	return chatType == models.ChatTypeGroup || chatType == models.ChatTypeSupergroup
+}
+
+func isAFKCommand(text string) bool {
+	return regexp.MustCompile(`^/\bafk\b|^\bbrb\b`).MatchString(text)
+}
+
+func handleReturnFromAFK(ctx context.Context, b *bot.Bot, update *models.Update, message *models.Message, userID int64) {
+	i18n := localization.Get(update)
+	_, duration, err := get_user_away(userID)
+	if err != nil && err != sql.ErrNoRows {
+		slog.Error("Couldn't get user away status", "UserID", userID, "Error", err.Error())
+		return
 	}
 
-	if message.Entities != nil {
-		for _, entity := range message.Entities {
-			if entity.Type == "mention" || entity.Type == "text_mention" {
-				if entity.Type == "text_mention" {
-					return entity.User.ID
-				}
+	if err := unset_user_away(userID); err != nil {
+		slog.Error("Couldn't unset user away status", "UserID", userID, "Error", err.Error())
+		return
+	}
 
-				username := message.Text[entity.Offset : entity.Offset+entity.Length]
-				userID, err := getIDFromUsername(username)
-				if err == nil {
-					return userID
-				}
+	humanizedDuration := localization.HumanizeTimeSince(duration, update)
 
-				slog.Error("Couldn't get user ID from username",
-					"Username", username,
-					"Error", err.Error(),
-				)
+	utils.SendMessage(ctx, b, message.Chat.ID, message.ID,
+		i18n("now-available", map[string]interface{}{
+			"userID":        userID,
+			"userFirstName": utils.EscapeHTML(message.From.FirstName),
+			"duration":      humanizedDuration,
+		}),
+		WithLargeMediaPreview,
+	)
+}
+
+func handleMentionedAFK(ctx context.Context, b *bot.Bot, update *models.Update, message *models.Message, mentionedUserID int64) {
+	i18n := localization.Get(update)
+	reason, duration, err := get_user_away(mentionedUserID)
+	if err != nil && err != sql.ErrNoRows {
+		slog.Error("Couldn't get user away status", "UserID", mentionedUserID, "Error", err.Error())
+		return
+	}
+
+	humanizedDuration := localization.HumanizeTimeSince(duration, update)
+
+	user, err := b.GetChat(ctx, &bot.GetChatParams{ChatID: mentionedUserID})
+	if err != nil {
+		slog.Error("Couldn't get user", "UserID", mentionedUserID, "Error", err.Error())
+		return
+	}
+
+	text := i18n("user-unavailable", map[string]interface{}{
+		"userID":        mentionedUserID,
+		"userFirstName": utils.EscapeHTML(user.FirstName),
+		"duration":      humanizedDuration,
+	})
+
+	if reason != "" {
+		text += "\n" + i18n("user-unavailable-reason", map[string]interface{}{
+			"reason": reason,
+		})
+	}
+
+	utils.SendMessage(ctx, b, message.Chat.ID, message.ID, text, WithLargeMediaPreview)
+}
+
+func getUserIDFromMessage(message *models.Message) int64 {
+	if m := message.ReplyToMessage; m != nil && m.From != nil {
+		return m.From.ID
+	}
+
+	for _, entity := range message.Entities {
+		if entity.Type == "text_mention" {
+			return entity.User.ID
+		}
+
+		if entity.Type == "mention" {
+			username := message.Text[entity.Offset : entity.Offset+entity.Length]
+			userID, err := getIDFromUsername(username)
+			if err == nil {
+				return userID
 			}
+
+			slog.Error("Couldn't get user ID from username", "Username", username, "Error", err.Error())
 		}
 	}
 
